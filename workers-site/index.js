60c812a05de07b2051a7af2a8ee01daf91d51537
Felipe Oduardo Sierra <felipe@solutionnine.com>
2022-07-06 08:55:23 -0400

    Adds cloudflare workers file

---
+++ workers-site/index.js
@@ -0,0 +1,18 @@
+import { getAssetFromKV, serveSinglePageApp } from '@cloudflare/kv-asset-handler';
+addEventListener("fetch", event => {
+  event.respondWith(handleEvent(event));
+});
+
+async function handleEvent(event) {
+  try {
+	return await getAssetFromKV(event, { mapRequestToAsset: serveSinglePageApp });
+
+  } catch (error) {
+	let pathname = new URL(event.request.url).pathname;
+	return new Response(`"${pathname}" not found`, {
+	  status: 404,
+	  statusText: "not found"
+	});
+  }
+ 
+}
